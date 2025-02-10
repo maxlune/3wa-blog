67738f8a545d024d2bb084832119e80e60f2362a
maxlune <85277579+maxlune@users.noreply.github.com>
2025-02-10 16:36:59 +0100

    added seeds for users

---
+++ seeders/20250210153447-demo-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        nickname: 'John',
+        password: '123',
+        email: 'example@example.com',
+        isContributor: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
