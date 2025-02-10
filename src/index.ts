524a787419397f66e555993edc42608322c512e1
maxlune <85277579+maxlune@users.noreply.github.com>
2025-02-10 14:33:28 +0100

    init express typescript fix nodemon

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+// app.get("/test", (req: Request, res: Response) => {
+//   res.send("Test");
+// });
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
